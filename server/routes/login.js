96adc28c483fd3111ceaaa526e6bc3d19661a22f
JDoliveros <joliveros@ieo.com.co>
2018-11-28 23:39:45 -0500

    Login: Se hace validacion de correo exstente y se valida que los password conisidan

--- server/routes/login.js
+++ server/routes/login.js
@@ -4,9 +4,37 @@ const Usuario = require('../models/usuario');
 const app = express();
 
 app.post('/login',(req,res)=>{
-    res.json({
-        ok:true
-    })
+
+    let body=req.body;
+
+    Usuario.findOne({email:body.email},(err,usuarioDb)=>{
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: err
+            })
+        }
+
+        if (!usuarioDb){
+            return res.status(400).json({
+                ok:false,
+                mensaje:'(Usuario) o contraseña incorrectos'
+            })
+        }
+
+        if(!bcrypt.compareSync(body.password,usuarioDb.password)){
+            return res.status(400).json({
+                ok:false,
+                mensaje:'Usuario o (contraseña) incorrectos'
+            })
+        }
+
+        res.json({
+            ok:true,
+            usuario:usuarioDb,
+            token:123
+        })
+    });
 });
 
 
