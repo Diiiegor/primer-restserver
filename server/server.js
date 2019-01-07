8f39c713c3d26ab84d9d75c2087fc08fc58d1425
juan diego oliveros <diiegooliveros@gmail.com>
2019-01-06 20:30:39 -0500

    Se agrega carpeta publica y signin de ggoogle

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path=require('path');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -12,6 +13,9 @@ app.use(bodyParser.json())
 
 //importo las rutas
 app.use(require('./routes/index'));
+
+//habilito la carpeta public
+app.use(express.static(path.resolve(__dirname,'../public')));
 
 //conexion a mongodb
 mongoose.connect(process.env.URLDB,{ useNewUrlParser: true },(error, res)=>{
