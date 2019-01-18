c9ec4c0e9a52d9d805040746b60c1bcaf2e2238f
Juan diego <diiegooliveros@gmail.com>
2019-01-18 18:26:07 -0500

    Se crea la ruta para buscar un producto por nombre

--- server/routes/producto.js
+++ server/routes/producto.js
@@ -73,4 +73,86 @@ app.get('/productos',verificaToken,(req,res)=>{
         })
 });
 
+//regresa un producto filtrado por su id
+app.get('/productos/:id',verificaToken,(req,res)=>{
+    let id=req.params.id;
+    Producto.findById(id,(error,producto)=>{
+        if (error){
+            return res.status(400).json({
+                ok:false,
+                mensaje:error
+            })
+        }
+        res.json({
+            ok:true,
+            producto
+        })
+    });
+});
+
+//actualizo un producto
+app.put('/productos/:id',verificaToken,(req,res)=>{
+    let id=req.params.id;
+    let body = req.body;
+    let usuario=req.usuario;
+    let producto={nombre, precioUni, descripcion, categoria} = body;
+    producto.usuario=usuario;
+
+    Producto.findByIdAndUpdate(id, producto, {new: true, runValidators: true},(error,producto)=>{
+        if (error){
+            return res.status(400).json({
+                ok:false,
+                mensaje:error
+            });
+        }
+        res.json({
+            ok:true,
+            producto
+        });
+    });
+
+});
+
+//elimino un producto
+app.delete('/productos/:id',verificaToken,(req,res)=>{
+    let id=req.params.id;
+    let usuario=req.usuario._id;
+
+    let body={disponible:false,usuario};
+    Producto.findByIdAndUpdate(id, body, {new: true, runValidators: true},(error,producto)=>{
+        if (error){
+            return res.status(400).json({
+                ok:false,
+                mensaje:error
+            });
+        }
+        res.json({
+            ok:true,
+            producto
+        });
+    })
+});
+
+//busvar productos
+app.get('/productos/buscar/:termino',verificaToken,(req,res)=>{
+    let termino=req.params.termino;
+    let regex=new RegExp(termino,'i');
+
+    Producto.find({nombre:regex})
+        .populate('categoria','nombre')
+        .exec((error,productos)=>{
+            if (error){
+                return res.status(400).json({
+                    ok:false,
+                    mensaje:error
+                })
+            }
+
+            res.json({
+                ok:true,
+                productos
+            })
+        })
+});
+
 module.exports = app;
